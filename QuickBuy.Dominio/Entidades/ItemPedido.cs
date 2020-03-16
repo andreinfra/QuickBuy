9b6e75eced42e3d90c8ba20f12760d6b328e555c
André Pereira <andrepereira@frwk.com.br>
2020-03-16 13:20:29 -0300

    Implementando Validação nas Classes ( Nada de Exceptions )

--- QuickBuy.Dominio/Entidades/ItemPedido.cs
+++ QuickBuy.Dominio/Entidades/ItemPedido.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
-
-namespace QuickBuy.Dominio.Entidades
+﻿namespace QuickBuy.Dominio.Entidades
 {
-    public class ItemPedido
+    public class ItemPedido : Entidade
     {
         public int Id { get; set; }
         public int ProdutoId { get; set; }
         public int Quantidade { get; set; }
+
+        public override void Validate()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
